59abe7e49d82742176a81f2498f15e117b597b9e
Nirmal122 <singhnirmal588@gmail.com>
2023-10-03 14:42:25 +0530

    DataBinding, Directives, Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularInterview';
+  isDisabled=true;
+
+  EventBinding(a:number,b:number){
+    console.log("Event Binding Testing");
+    console.log(a+b);
+  }
+
+  name="";
+
+  students=["Ben Stokes", "Jos Buttler","Steve Smith"]
+
+  bgColor='black'
+  color='white'
+
+  className= "bwClass"
+
+  str="Nirmal Singh"
+
+  dob="12/08/1997"
 }
