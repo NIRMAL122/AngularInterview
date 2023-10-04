2cc04ac47597577cbc02703f39eda44f1461160b
Nirmal122 <singhnirmal588@gmail.com>
2023-10-04 09:58:16 +0530

    Observable, HTTPClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { LoggingService } from './Services/logging.service';
+import {Observable} from 'rxjs';
+import { StudentService } from './Services/student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[LoggingService]
 })
 export class AppComponent {
   title = 'AngularInterview';
@@ -15,15 +19,43 @@ export class AppComponent {
   }
 
   name="";
-
   students=["Ben Stokes", "Jos Buttler","Steve Smith"]
-
   bgColor='black'
   color='white'
-
   className= "bwClass"
-
   str="Nirmal Singh"
-
   dob="12/08/1997"
+
+  //Promise
+  isPromis= new Promise<string>((resolve,reject)=>{
+    console.log("Promise Start");
+  })
+
+  //observable
+  myObservable= new Observable(observer=>{
+    console.log("Observable Start");
+    observer.next("a");
+    observer.next("b");
+    observer.next("c");
+  })
+
+  public studentsJSON:any[]=[];
+
+  constructor(private _logging:LoggingService,
+              private _studentService: StudentService){
+    _logging.logError();
+  }
+
+  ngOnInit():void
+  {
+    //observable subscription
+    this.myObservable.subscribe((val=>{
+      console.log(val);
+    }));
+
+    this._studentService.getStudents().subscribe(
+      data=>this.studentsJSON=data
+    )
+  }
+
 }
